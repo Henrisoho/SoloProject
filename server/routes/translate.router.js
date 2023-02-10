a0533fa865afddcd4b4b63ea2057ec7a9f8c028f
henrisoho <bakingwithmymother@gmail.com>
2023-02-09 22:01:11 -0600

    unlincked

---
+++ server/routes/translate.router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const router = express.Router()
+const axios = require('axios');
+
+router.post('/', rejectUnauthenticated, (req, res) =>{
+const string = (req.query.word)
+console.log(string)
+const encodedParams = new URLSearchParams();
+encodedParams.append("q", string);
+encodedParams.append("target", "fr");
+encodedParams.append("source", "en");
+
+const options = {
+  method: 'POST',
+  url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': process.env.API_KEY,
+    'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+  },
+  data: encodedParams
+};
+
+axios.request(options).then(function (response) {
+   res.send(response.data.data.translations[0].translatedText)
+}).catch(function (error) {
+    console.error(error);
+});
+})
+
+module.exports = router
